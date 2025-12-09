be162c8aa935ea2bb054a4fe3acc57fccf10d871
ams-al <58682318+ams-al@users.noreply.github.com>
2025-12-09 14:38:46 +0100

    zojz

---
+++ config/system-addons.php
@@ -0,0 +1,66 @@
+<?php return array (
+  'admin_panel' => 
+  array (
+    'active' => '1',
+    'name' => NULL,
+    'identifier' => NULL,
+    'username' => 'amshosting',
+    'purchase_key' => '38a0b967-dfdd-41b3-9d07-698e75beda28',
+    'software_id' => 'MzE0NDg1OTc=',
+    'domain' => 'zojz.al',
+    'software_type' => 'product',
+    'errors' => 
+    array (
+    ),
+  ),
+  'vendor_panel' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'user_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'vendor_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'deliveryman_app' => 
+  array (
+    'active' => '1',
+    'name' => 'Kaseem Gibbs',
+    'identifier' => 'tewuqozum@mailinator.com',
+    'username' => 'gutewag',
+    'purchase_key' => 'Accusantium blanditi',
+    'software_id' => 'MzYwODUwNDE=',
+    'domain' => '127.0.0.1:8000',
+    'software_type' => 'addon',
+    'errors' => 
+    array (
+    ),
+  ),
+  'theme_lifestyle' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+);
